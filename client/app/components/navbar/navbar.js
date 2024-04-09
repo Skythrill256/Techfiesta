64aa7fb63b5d2883db80c43b947eb111ab63029c
IndraniSom <indranisom39@gmail.com>
2024-04-09 09:46:14 +0530

    added styling

--- client/app/components/navbar/navbar.js
+++ client/app/components/navbar/navbar.js
@@ -1,25 +1,47 @@
 import React from 'react'
+import Image from 'next/image'
+import Home from '../../../assets/home.svg'
+import Transaction from '../../../assets/transaction.svg'
+import Profile from '../../../assets/profile.svg'
 import './navbar.css'
-const navbar = () => {
+const navbar = (props) => {
   return (
-    <div className="w-screen container ">
+    <div className="nav">
 	<ul>
 	<li>
-		<a href="#">
-			<i className="ai-home"></i>
-			<span>home icon</span>
+		<a href={props.homeLink}>
+			
+		<i ><Image
+      src={Home}
+      width={50}
+      height={50}
+	  alt='home'
+      className=' '
+      /></i>
 		</a>
 	</li>
 	<li>
-		<a href="#">
-			<i className="ai-image"></i>
-			<span>tansaction icon</span>
+		<a href={props.transactionLink}>
+			<i ><Image
+      src={Transaction}
+      width={100}
+      height={100}
+	  alt='home'
+      className=' '
+      /></i>
+			
 		</a>
 	</li>	
 	<li>
-		<a href="#">
-			<i className="ai-file"></i>
-			<span>profile icon</span>
+		<a href={props.profileLink}>
+			<i><Image
+      src={Profile}
+      width={50}
+      height={50}
+      className=' '
+	  alt='home'
+      /></i>
+			
 		</a>
 	</li>
 	
--- client/app/sourceprofile/page.js
+++ client/app/sourceprofile/page.js
@@ -7,27 +7,33 @@ import Navbar from '../components/navbar/navbar.js'
 import Button from  '../components/button/button'
 const page = () => {
   return (
-    <div className='maincontainer'>
+    <div className='maincontainer '>
+      <div className='flex flex-col items-center mb-8 mt-10'>
+
       <Image
       src={Source}
-      width={500}
-      height={500}
-      className='source'
-    />
+      width={300}
+      height={300}
+      className='source  sm:w-3/4 md:w-1/2 lg:w-1/3 xl:w-1/4'
+      />
     <Image
       src={Profile}
       width={200}
       height={180}
-      className='profile'
-    />
+      className='profile mt-10 '
+      />
+      </div>
       
-      <div className='container'>
+      <div className='my-8  flex flex-col items-center container '>
         <label>Add Name</label>
         <input type='text' className='input' placeholder='Enter the details'/>
         <label>Add Location</label>
         <input type='text' className='input' placeholder='Enter the details'/>
+        <label>Add Email</label>
+        <input type='email' className='input' placeholder='Enter the details'/>
+        
       </div>
-      <a href='/sourceregister'><Button title="Add"/></a>
+     <Button title="Add"/>
       <Navbar/>
     </div>
     
